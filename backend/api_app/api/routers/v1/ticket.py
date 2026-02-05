1e95e7edf7b5d1467b08d57a9f7e581d2661fd09
PunPK <phurutsakorn.kps@gmail.com>
2026-02-05 16:35:33 +0700

    add: check in ticket user wuth raspi

--- backend/api_app/api/routers/v1/ticket.py
+++ backend/api_app/api/routers/v1/ticket.py
@@ -8,7 +8,11 @@ from api_app.api.core import dependencies
 from api_app.api.core.redis import get_redis
 from redis.asyncio import Redis
 from api_app.api.utils.google import pubsub_client
+from datetime import datetime
+from bson import ObjectId
 
+from api_app import models
+from api_app.api.core.redis import get_redis
 
 router = APIRouter(prefix="/tickets", tags=["tickets"])
 
@@ -74,3 +78,63 @@ async def check_sync_status(
     """
     service = TicketBookingService(redis)
     return await service.check_inventory_sync(event_id)
+
+@router.post("/check-in/{ticket_id}")
+async def check_in_ticket(
+    ticket_id: str,
+    redis: Redis = Depends(get_redis),
+):
+    """
+    Check-in ticket by QR scan
+    """
+
+    lock_key = f"lock:ticket:{ticket_id}"
+    lock = await redis.set(lock_key, "1", ex=5, nx=True)
+
+    if not lock:
+        raise HTTPException(status_code=429, detail="Ticket is being processed")
+
+    try:
+        ticket = await models.UserTicket.find_one(
+            models.UserTicket.id == ObjectId(ticket_id)
+        )
+
+        if not ticket:
+            return {"status": "invalid", "message": "Ticket not found "}
+
+        # ยกเลิก / คืนเงิน / หมดอายุ
+        if ticket.status != "booked":
+            return {"status": "invalid", "message": f"Ticket status = {ticket.status}"}
+
+        # สแกนซ้ำ
+        if ticket.is_checked_in:
+            return {
+                "status": "already_used",
+                "message": "Ticket already checked in ",
+                "checked_in_at": ticket.checked_in_date,
+            }
+
+        now = datetime.now()
+
+        # # สแกนก่อนเวลางาน
+        # if now < ticket.event_start_date:
+        #     return {"status": "too_early", "message": "Event has not started yet"}
+
+        # # สแกนหลังงานจบ
+        # if now > ticket.event_end_date:
+        #     return {"status": "expired", "message": "Event has ended"}
+
+        ticket.is_checked_in = True
+        ticket.checked_in_date = now
+        await ticket.save()
+
+        return {
+            "status": "success",
+            "message": "Check-in successful",
+            "ticket_id": ticket_id,
+            "ticket_name": ticket.ticket_name,
+            "checked_in_at": now,
+        }
+
+    finally:
+        await redis.delete(lock_key)
