ccda36344c7f018cda1c76c4261bece0e40af0cd
6710110151 <6710110151@psu.ac.th>
2026-02-05 19:29:00 +0700

    refactor: change status error

--- frontend/src/routes/+error.svelte
+++ frontend/src/routes/+error.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
 </script>
 
 <div class="flex flex-col text-center h-screen">
 	<div class="mx-auto my-auto flex flex-col gap-5">
-		<p class="text-4xl">{$page.status} {$page.error?.message}</p>
+		<p class="text-4xl">{page.status} {page.error?.message}</p>
 		<div>
 			<button onclick={() => history.go(-1)} class="btn btn-primary text-white"> Go Back </button>
 		</div>
